1b9f89dc5ed1d1684686e009e513983f03c3f2bf
“pragyann7” <146330072+pragyann7@users.noreply.github.com>
2025-09-26 15:14:52 +0545

    Home page only

---
+++ khelu-ki/Components/Home.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div className="flex flex-col min-h-screen bg-gray-100">
+            {/* Main content */}
+            <div className="flex flex-col items-center justify-center flex-grow">
+                <h1 className="text-center text-5xl mb-4 game-heading">Khelū̃ Ki?</h1>
+                <h2 className="text-center mb-13">Should I play?</h2>
+
+                <div className="flex flex-col items-center gap-4">
+                    <button className="py-2 px-7 bg-black text-white rounded-2xl hover:scale-105 transition-transform">
+                        Taas
+                    </button>
+                    <button className="py-2 px-7 bg-black text-white rounded-2xl hover:scale-105 transition-transform">
+                        Langur burja
+                    </button>
+                </div>
+            </div>
+
+            {/* Footer */}
+            <footer className="text-center py-4 bg-gray-900 text-white">
+                © 2025 Pragyan / Developer
+            </footer>
+        </div>
+    )
+}
+
+export default Home
